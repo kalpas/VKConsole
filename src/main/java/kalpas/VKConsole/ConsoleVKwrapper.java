050e291ae9e9cd910a0113744ae87bf4ce617668
andrey.arnautov <puffdrew@gmail.com>
2013-01-20 19:07:40 +0200

    working assembly. bat file added to start application

--- src/main/java/kalpas/VKConsole/ConsoleVKwrapper.java
+++ src/main/java/kalpas/VKConsole/ConsoleVKwrapper.java
@@ -1,43 +1,106 @@
 package kalpas.VKConsole;
 
+import kalpas.VKCore.VKModule;
+import kalpas.VKCore.simple.DO.User;
+import kalpas.VKCore.simple.helper.GMLHelper;
+import kalpas.VKCore.simple.helper.HttpClientContainer;
+import kalpas.VKCore.stats.GroupStats;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.joda.time.DateTime;
+
+import com.google.common.collect.Multimap;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 
 /**
  * VKConsole
  * 
  */
+@SuppressWarnings("static-access")
 public class ConsoleVKwrapper {
     private static Logger            logger    = LogManager.getLogger(ConsoleVKwrapper.class.getName());
+
     private static HelpFormatter     formatter = new HelpFormatter();
     private static CommandLineParser parser    = new PosixParser();
-    private static Options           options   = createOptions();
+
+    private static Options           options   = null;
+
+    // Options
+    private static Option            graph     = null;
+    private static Option            user      = null;
+    private static Option            group     = null;
+    private static Option            file      = null;
+
+    private static Injector          injector  = Guice.createInjector(new VKModule());
 
     public static void main(String[] args) {
+        options = createOptions();
         CommandLine line = null;
+
         try {
             line = parser.parse(options, args);
-            
+            if (line.getOptions().length == 0) {
+                throw new IllegalArgumentException("no options passed. pls see usage");
+            }
+
+            if (line.hasOption(graph.getOpt())) {
+                createGraph(line);
+            } else {
+                logger.info("no options was passed to select activity\ngraph would be created");
+                createGraph(line);
+            }
+
         } catch (ParseException | IllegalArgumentException e) {
+            logger.error(e.getMessage());
             formatter.printHelp("VK", options);
+        }
+    }
+
+    private static void createGraph(CommandLine line) {
+        if (line.getOptionValue(group.getOpt()) != null) {
+
+            String gid = line.getOptionValue(group.getOpt());
+            String fileName = line.getOptionValue(file.getOpt());
+            if (fileName == null) {
+                fileName = "club" + gid + "timestamp" + new DateTime().getMillis();
+                logger.warn("file name wasn't specified. generating default - {}", fileName);
+            }
+
+            GroupStats stats = injector.getInstance(GroupStats.class);
+            Multimap<User, User> multimap = stats.getMemberNetwork(gid);
+            GMLHelper.writeToFile(fileName, multimap);
+            injector.getInstance(HttpClientContainer.class).shutdown();
+
+        } else if (line.getOptionValues(user.getOpt()) != null) {
+            throw new UnsupportedOperationException("getting user graph is not implemented yet");
+        } else {
+            throw new IllegalArgumentException("neither group id nor user id were specified");
         }
     }
 
     private static Options createOptions() {
         Options options = new Options();
-        Option graph = new Option("G", "graph", false, "generate connections graph");
-        Option user = new Option("u", "user", true, "user id");
-        Option group = new Option("g", "group", true, "group id");
+
+        graph = new Option("G", "graph", false, "generate connections graph");
+        user = OptionBuilder.withArgName("uid").hasArg().withDescription("user id").withLongOpt("user").create("u");
+        group = OptionBuilder.withArgName("gid").hasArg().withDescription("group id").withLongOpt("group").create("g");
+        file = OptionBuilder.withArgName("file").hasArg().withDescription("output file").withLongOpt("file")
+                .create("f");
+
         options.addOption(group);
         options.addOption(user);
         options.addOption(graph);
+        options.addOption(file);
         return options;
     }
 }
